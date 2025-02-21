88c4b39bf66709dbd73284ba3dac434f94d3cc4a
Aleksa Stojanovic <aleksastojanovic18@gmail.com>
2025-02-21 17:00:31 +0100

    Trying to fix hosting on renderer

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from datetime import datetime
 from pymongo import MongoClient
 from flask import Flask, jsonify
@@ -11,18 +12,25 @@ load_dotenv()
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+
 @app.route('/', methods=['GET'])
 def service():
     #We take todays date and format it to the needed format
     DTnow = datetime.now() 
     DTformatted = DTnow.strftime("%Y-%m-%d")
+    logging.info(f"The day used for fetching data is {DTformatted}")
     # DTformatted = "2025-02-20"
     #Fetches games based on the date
     res = fetchGames(DTformatted)
 
     if res is None:
+        logging.error("The result from fetching is None")
         return jsonify({"error": "Response is None"}), 404
-
+    else:
+        logging.info("The fetched data is not None")
+        
     #Formats all the data
     propsToDelete = [  
                 "league",
@@ -64,14 +72,18 @@ def service():
             result = collection.insert_many(filterGames)
 
             if result.acknowledged:
+                logging.info("Successfuly saved!")
                 return jsonify({"success": "Success"}), 200
             else:
+                logging.error("There has been an erorr with saving the results to mongoDb!")
                 return jsonify({"error": "Failed!"})
 
         else: 
+            logging.info("The games for the day have already been saved!")
             return jsonify({"success": "No new games"})
 
     except Exception as e:
+        logging.error(f"There has been an exception! {e}")
         return jsonify({"error": f"Error: {e}"})
     finally:
         client.close()
@@ -81,5 +93,5 @@ def service():
 if __name__ == '__main__':
     from waitress import serve
     # app.run(debug=True)
-    # port = int(os.environ.get("PORT", 8008))
-    serve(app, host="0.0.0.0", port=8008)
+    port = int(os.environ.get("PORT", 8008))
+    serve(app, host="0.0.0.0", port=port)
